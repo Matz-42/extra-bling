2c2200d712042d299755f064e367dc81bf4a217d
Matzie <fake@mail.com>
2023-06-03 13:36:36 +0200

    Nerfed Recycling Machine to 2 gold

--- scripts/symbol_recycling_machine.gd
+++ scripts/symbol_recycling_machine.gd
@@ -5,7 +5,7 @@ func _init():
 	type = "recycling_machine"
 	display_name = "Recycling Machine"
 	localized_names = {}
-	value = 3
+	value = 2
 	description = "Gives <icon_reroll_token><color_49AA10><value_2><end> when an adjacent symbol is <color_E14A68>destroyed<end>. <color_E14A68>Destroys<end> itself after giving <icon_reroll_token><color_49AA10><value_1><end>."
 	localized_descriptions = {}
 	rarity = "rare"
