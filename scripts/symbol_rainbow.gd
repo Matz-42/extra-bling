028dd0270ff458b95538f6052486563e467109aa
Matzie <fake@mail.com>
2023-06-05 10:13:18 +0200

    Rainbow no longer buffs coins

--- scripts/symbol_rainbow.gd
+++ scripts/symbol_rainbow.gd
@@ -6,7 +6,7 @@ func _init():
 	display_name = "Rainbow"
 	localized_names = {}
 	value = 3
-	description = "Adjacent <icon_hover_coin> <icon_bubble> and <icon_rain> act like <icon_wildcard> and give <color_E14A68><value_1>x<end> more <icon_coin>. Affects non-adjacent symbols as well if you have <icon_clear_sky>."
+	description = "Adjacent <icon_bubble> and <icon_rain> act like <icon_wildcard> and give <color_E14A68><value_1>x<end> more <icon_coin>. Affects non-adjacent symbols as well if you have <icon_clear_sky>."
 	localized_descriptions = {}
 	values = [2]
 	rarity = "rare"
@@ -17,33 +17,6 @@ func _init():
 			"comparisons": [
 				{
 					"a": "type",
-					"b": "coin"
-				}
-			],
-			"value_to_change": "wildcarded",
-			"diff": true,
-			"anim": "circle",
-			"anim_targets": "adjacent_symbol",
-			"sfx_override": "ding"
-		},
-		{
-			"forbidden_items": ["clear_sky"],
-			"effect_type": "adjacent_symbols",
-			"comparisons": [
-				{
-					"a": "type",
-					"b": "coin"
-				}
-			],
-			"value_to_change": "value_multiplier",
-			"diff": values[0]
-		},
-		{
-			"forbidden_items": ["clear_sky"],
-			"effect_type": "adjacent_symbols",
-			"comparisons": [
-				{
-					"a": "type",
 					"b": "bubble"
 				}
 			],
@@ -92,35 +65,8 @@ func _init():
 			"value_to_change": "value_multiplier",
 			"diff": values[0]
 		},
-		
-		
-		{
-			"required_items": ["clear_sky"],
-			"effect_type": "symbols",
-			"comparisons": [
-				{
-					"a": "type",
-					"b": "coin"
-				}
-			],
-			"value_to_change": "wildcarded",
-			"diff": true,
-			"anim": "circle",
-			"anim_targets": "adjacent_symbol",
-			"sfx_override": "ding"
-		},
-		{
-			"required_items": ["clear_sky"],
-			"effect_type": "symbols",
-			"comparisons": [
-				{
-					"a": "type",
-					"b": "coin"
-				}
-			],
-			"value_to_change": "value_multiplier",
-			"diff": values[0]
-		},
+
+
 		{
 			"required_items": ["clear_sky"],
 			"effect_type": "symbols",
