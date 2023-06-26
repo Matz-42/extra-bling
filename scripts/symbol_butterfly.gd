3799edb8032d298fd61718cc0b6d75d85e7084d4
Matzie <fake@mail.com>
2023-06-26 17:47:59 +0200

    Added Butterfly, more than likely will be slightly tweaked later

---
+++ scripts/symbol_butterfly.gd
@@ -0,0 +1,30 @@
+extends "res://Mod Data.gd"
+
+func _init():
+	mod_type = "symbol"
+	type = "butterfly"
+	display_name = "Butterfly"
+	localized_names = {}
+	value = 4
+	description = "Has a <color_E14A68><value_1>%<end> chance of giving <icon_essence_token><color_FF005D><value_2><end>."
+	localized_descriptions = {}
+	values = [8,1]
+	rarity = "very_rare"
+	groups = ["organism","animal","triggerchance"]
+	sfx = ["shine"]
+	effects = [
+		{
+			"comparisons": [
+				{
+					"a": "values",
+					"b": {"starting_value": {"rand_num": {"min": 0, "max": 100}}},
+					"rand": true,
+					"value_num": 0
+				}
+			],
+			"anim": "circle",
+			"sfx_override": "shine",
+			"value_to_change": "essence_token_value",
+			"diff": values[1]
+		}
+	]
