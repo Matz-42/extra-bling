61c812d30d2121f927f29832d9523a571eac324b
Matzie <fake@mail.com>
2023-08-19 03:47:08 +0200

    Blender: changed banana to use groups

---
+++ scripts/group_banana.gd
@@ -0,0 +1,13 @@
+extends "res://Mod Data.gd"
+
+func _init():
+	mod_type = "existing_symbol"
+	type = "banana"
+	display_name = "Banana"
+	inherit_effects = true
+	inherit_art = true
+	inherit_groups = true
+	inherit_description = true
+	rarity = "common"
+	value = 1
+	groups = ["blendermixed"]
